faa6e6aeb275716e9477b32c8e553bd2be0982d5
Dreijers <dzims.edijs.dreijers@accenture.com>
2019-09-26 15:31:40 +0300

    Added functionality to delete tasks from UI and database

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,31 +1,55 @@
-function populateTable(description, isCompleted){
-    $("table").append("<tr>" + "<td> " + description + "</td>" + "<td> " + isCompleted + "</td>" + "</tr>");
-}
+$(document).ready(function() {
 
-function retrieveTasks(){
-    $.getJSON( "/tasks", function( data ) {
-      $.each( data, function( key, val ) {
-        populateTable(val.description, val.completed);
-      });
+    function addRow(val) {
+        $("table").append("<tbody><tr>"
+        + "<td> " + val.id + "</td>"
+        + "<td> " + val.description + "</td>"
+        + "<td> " + val.completed + "<button type='button' id='btn-delete' class='btn btn-sm btn-outline-danger float-right'>X</button>" +"</td>"
+        + "</tbody></tr>");
+    }
+
+    function retrieveTasks(){
+        $.getJSON( "/tasks", function( data ) {
+            $.each(data, function( key, val ) {
+               addRow(val);
+            });
+        });
+    }
+
+    retrieveTasks();
+
+    $(document).on("click", "#btn-delete" , function(){
+        var currentTr = $(this).closest('tr');
+        var taskId = currentTr.children("td:first").text();
+        $(this).closest('tr').remove();
+            $.ajax({
+              type: "DELETE",
+              url: "/tasks/deleteTask/" + taskId,
+              success: function(data) { alert('data: ' + data); },
+              dataType: "json"
+        });
     });
-}
 
-$("#button").click(function(){
-    var tasksDescription = $("#task-description").val();
-    populateTable(tasksDescription, false);
-    $.ajax({
-    headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-      type: "POST",
-      url: "/tasks/createTask",
-      data: '{"description":"' + tasksDescription + '","completed":false}',
-      success: function(data) { alert('data: ' + data); },
-      dataType: "json"
+
+    $("#btn-create").click(function(){
+        var tasksDescription = $("#task-description").val();
+        var json;
+        $.ajax({
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            url: "/tasks/createTask",
+            data: '{"description":"' + tasksDescription + '","completed":false}',
+            complete: function (result) {
+                $.getJSON( "/tasks", function( data ) {
+                    json = data;
+                    var currentTasksId = json[json.length - 1].id;
+                    addRow({id: currentTasksId, description:tasksDescription, completed:false});
+                });
+            },
+            dataType: "json"
+        });
     });
 });
-
-window.onload = function() {
-  retrieveTasks();
-};
