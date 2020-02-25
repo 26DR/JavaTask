a13a8b1ce9890858ef6d9df50902a7cc0c44c3e0
Dzims Edijs Dreijers <edijs211@yahoo.com>
2020-02-25 07:17:15 +0200

    Integration tests

--- src/test/java/com/example/javatask/controller/TaskRestControllerIntegrationTest.java
+++ src/test/java/com/example/javatask/controller/TaskRestControllerIntegrationTest.java
@@ -8,9 +8,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,17 +23,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {TaskRestController.class, TaskService.class, TaskRestControllerTest.TestConfig.class})
 @WebAppConfiguration
-public class TaskRestControllerTest {
+public class TaskRestControllerIntegrationTest {
 
     private MockMvc mockMvc;
 
@@ -52,64 +49,41 @@ public class TaskRestControllerTest {
     }
 
     @Test
-    public void getTaskListOk() throws Exception {
-
-        List<Task> listOfTasks = Arrays.asList(new Task(1, "Drink coffee", true),
-                new Task(2, "Do Java task", true));
-
-        when(taskService.findAllTasks())
-                .thenReturn(listOfTasks);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/tasks")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(asJsonString(listOfTasks)));
-    }
-
-    @Test
-    public void getTaskListNotFound() throws Exception {
-        when(taskService.findAllTasks())
-                .thenReturn(null);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/tasks")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    public void createTaskMock() throws Exception {
-        Task taskToSave = new Task(1, "Drink coffee", false);
-
-        when(taskService.saveOrUpdateTask(taskToSave))
-                .thenReturn(taskToSave);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/tasks")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(asJsonString(taskToSave)))
-                .andExpect(status().isCreated());
-    }
-
-    @Test
     public void createTask() throws Exception {
         Task taskToSave = new Task(1, "Drink coffee", false);
 
         mockMvc.perform(MockMvcRequestBuilders
                 .post("/tasks")
-                .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(asJsonString(taskToSave)))
-                .andExpect(status().isCreated())
-                .andDo(print());
+                .andExpect(status().isCreated());
+
+        assertEquals(taskToSave, taskService.findById(1L).get());
+    }
+
+    @Test
+    public void retrieveAllTasks() throws Exception {
+
+        List<Task> expectedListOfTasks = Arrays.asList(new Task(1, "Drink coffee", true),
+                new Task(2, "Do Java task", false),
+                new Task(3, "Try not to fall asleep", false),
+                new Task(4, "Go home", false));
+
+        assertEquals(expectedListOfTasks, taskService.findAllTasks());
     }
 
     @Test
     public void deleteTask() throws Exception {
+
+        List<Task> expectedListOfTasks = Arrays.asList(new Task(2, "Do Java task", false),
+                new Task(3, "Try not to fall asleep", false),
+                new Task(4, "Go home", false));
+
         mockMvc.perform(MockMvcRequestBuilders
                 .delete("/tasks/{id}", 1))
                 .andExpect(status().isOk());
+
+        assertEquals(expectedListOfTasks, taskService.findAllTasks());
     }
 
     /*auxiliary methods*/
